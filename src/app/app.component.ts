4cc3e2043aa21c8c82a876ee2d6e379b514857a6
Kövesdi Ákos <kovesdiakos@ktch.hu>
2025-06-11 10:31:37 +0200

    Kész

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  radius !:number
+  height !:number
+  volume !:number
+
+  startCalc(){
+    this.volume = 1.0/3.0 * Math.pow(this.radius,2) * Math.PI * this.height;
+  }
+}
